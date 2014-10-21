19d972a246dc5c39db38a03af5cad12ce353a4ea
olyasaint <olya@ostryk.com>
2014-10-21 16:12:16 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Write your very own for loop!
+write a for loop that gets the computer to count down from 100 until 0 by 5. This time, make sure not to print 0.
+
+for (var i = 100; i > 0; i-=5) {
+    console.log (i);
+}
